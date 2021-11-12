253e268108acb658ba2efeaa4e89452391ce038e
Kai <kai.wissler96@gmail.com>
2021-11-12 15:49:36 +0100

    feat: add bg animation to welcome chapter

--- components/chapters/welcome.tsx
+++ components/chapters/welcome.tsx
@@ -1,6 +1,33 @@
+import { useContext, useRef, useEffect } from "react";
+import { BG_COLORS } from "../../lib/constants";
+import { AnimationContext } from "../../lib/context/animation-context";
+
 export const WelcomeChapter = () => {
+  const { gsap } = useContext(AnimationContext);
+  const chapter = useRef<HTMLDivElement | null>();
+
+  useEffect(() => {
+    if (chapter.current) {
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: chapter.current,
+            start: "top bottom",
+            end: "bottom bottom",
+            scrub: true,
+          },
+        })
+        .from("main", {
+          backgroundColor: BG_COLORS[0],
+        })
+        .to("main", {
+          backgroundColor: BG_COLORS[0],
+        });
+    }
+  }, [gsap]);
+
   return (
-    <div className="container" id="welcomeContainer">
+    <div className="container" ref={(el) => chapter.current = el}>
       <h1>Kai Wißler</h1>
       <h2 className="headline-3">Web Developer</h2>
       <i className="arrow down"></i>
