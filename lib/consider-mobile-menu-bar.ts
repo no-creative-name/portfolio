2654bf4ea5b209b6ec5a76dfbe58606f47b81ccb
Kai Wissler <kai.wissler@modulace.com>
2026-08-04 09:20:40 +0200

    Replace old portfolio

--- lib/consider-mobile-menu-bar.ts
+++
@@ -1,9 +0,0 @@
-export const considerMobileMenuBar = () => {
-	let vh = window.innerHeight * 0.01;
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-	window.addEventListener('resize', () => {
-		let vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-	});
-}
